b4700ca52ef556e70eacc5cba5b7dcb59eaf631e
whitewizardd <o.samuel@native.semicolon.africa>
2024-02-28 11:16:56 +0100

    continuation on the comment contract

--- contracts/Comment.sol
+++ contracts/Comment.sol
@@ -17,21 +17,29 @@ contract Comment{
 
     Like private like;
 
+    uint private id;
+
     constructor(){
         like = new Like();
     }
 
-    mapping (address => mapping (uint => uint)) private postCounts;
-    mapping (address => mapping (uint => NewComment[])) private comments;
+    mapping (uint => NewComment[]) private comments;
 
 
-    function createComment(address postOwner, address commenter, uint id, string calldata _comment) external {
-        NewComment storage comment = comments[postOwner][id][postCounts[postOwner][id]];
-        comment.comment = _comment;
-        comment.commentedTime = block.timestamp;
-        comment.user = commenter;
-        comment.id = postCounts[postOwner][id];
+    function createComment(address commenter, uint postId, string calldata _comment) external {
+        NewComment memory comment = NewComment(id++, commenter, _comment, block.timestamp);
+        comments[postId].push(comment);
+    }
 
-        postCounts[postOwner][id] = postCounts[postOwner][id] + 1;
+    function getPostComments(uint postId) external view returns(NewComment[] memory){
+        return comments[postId];
+    }
+
+    function togglePostCommentLike(uint postId, uint commentId, address user) external returns(bool) {
+        return like.toggleLikeCommentPost(postId, commentId, user);
+    }
+
+    function getPostCommentLikesCount(uint postId, uint commentId) external view returns (uint){
+        return like.getPostCommentLikesCount(postId, commentId);
     }
 }
